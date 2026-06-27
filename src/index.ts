5e7afe5927e9e05253e55cffffa6a4949800f58e
supertms <elfmoon@vip.qq.com>
2026-06-27 18:11:43 +0800

    add public exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { generateZiweiChart } from "./ziwei/iztroAdapter.js";
+export type { ZiweiChart, ZiweiPalace, ZiweiProfile } from "./ziwei/types.js";
+export { ZiweiInputSchema } from "./shared/schema.js";
+export type { ZiweiInput } from "./shared/schema.js";
